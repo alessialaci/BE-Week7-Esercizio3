1bb60564a4749d6b0985b74e41005acfd99b0542
Alessia Lacitignola <80158378+alessialaci@users.noreply.github.com>
2023-02-22 19:31:40 +0100

    Add files via upload

---
+++ Es22_02/src/main/java/it/lucal/epicode/es22_02/App.java
@@ -0,0 +1,10 @@
+package it.lucal.epicode.es22_02;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("NON CAMBIATE I GRUPPI PER FAVORE!!!!");
+	}
+
+}
